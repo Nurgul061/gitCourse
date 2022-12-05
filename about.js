01c89edb28adfad9aa197a4562f82564b391f478
Nurgul061 <119525325+Nurgul061@users.noreply.github.com>
2022-12-05 21:36:00 +0300

    fifth

---
+++ about.js
@@ -0,0 +1,27 @@
+const anchors = document.querySelectorAll('a[href*="#down1"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+let text = 'BELIEVE   in   CRAFTSMANSHIP  and    LUXURIOUS   DESIGN'
+
+let content = document.querySelector('.citation')
+
+for (let i in [...text]) {
+  let letter = document.createElement('span')
+  letter.textContent = [...text][i]
+  if(letter.textContent.match(/\s/)) {
+    letter.style.margin = 'auto 3px'
+  }
+  letter.style.animationDelay = i/10+'s'
+  content.appendChild(letter)
+}
