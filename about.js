ec1448027fd6a0dc377b2facfd85ce65f9bb1e57
Nurgul061 <119525325+Nurgul061@users.noreply.github.com>
2022-12-07 00:30:07 +0300

    sixth

--- about.js
+++ about.js
@@ -12,16 +12,25 @@ for (let anchor of anchors) {
     })
   })
 }
-let text = 'BELIEVE   in   CRAFTSMANSHIP  and    LUXURIOUS   DESIGN'
 
-let content = document.querySelector('.citation')
-
-for (let i in [...text]) {
-  let letter = document.createElement('span')
-  letter.textContent = [...text][i]
-  if(letter.textContent.match(/\s/)) {
-    letter.style.margin = 'auto 3px'
-  }
-  letter.style.animationDelay = i/10+'s'
-  content.appendChild(letter)
+function change(ex){
+      document.getElementById("label1").innerHTML="Detailed <br/><em>Craftsmanship</em>";
+      document.getElementById("label").innerHTML="Because of our expertise and passionate participation throughout the entire process we are able to find the perfect balance between the contemporary aesthetic and the belonged quality.";
+      document.getElementById("ch2").style.color="lightgrey";
+      document.getElementById("ch3").style.color="lightgrey";
+      document.getElementById("ch1").style.color="black";
+}
+function change2(ex){
+  document.getElementById("label").innerHTML="We commit ourselves to move fast in a constantly changing fashion landscape and to incorporate the most relevant trends into our own signature mix of classic and modern elements."
+  document.getElementById("label1").innerHTML="Design<br/><em>Driven</em>";
+  document.getElementById("ch1").style.color="lightgrey";
+  document.getElementById("ch3").style.color="lightgrey";
+  document.getElementById("ch2").style.color="black";
+}
+function change3(ex){
+  document.getElementById("label").innerHTML="Since 2022 we aim at creating luxurious and contemporary fashion goods for wearable prices, because we believe that every woman should be able to indulge herself in luxury."
+  document.getElementById("label1").innerHTML="Affordable<br/><em>Luxury</em>";
+  document.getElementById("ch1").style.color="lightgrey";
+  document.getElementById("ch2").style.color="lightgrey";
+  document.getElementById("ch3").style.color="black";
 }
