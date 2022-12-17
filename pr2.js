f90ede013304e674263ff7f7ec3fd9ed05462ac6
Aisulu44 <119520764+Aisulu44@users.noreply.github.com>
2022-12-18 00:33:12 +0600

    Add files via upload

---
+++ pr2.js
@@ -0,0 +1,65 @@
+function changeBg(){
+    var navbar = document.getElementById('navbar');
+    var scrollvalue = window.scrollY;
+    if(scrollvalue<100){
+      navbar.classList.remove('bgColor');
+    }else{
+      navbar.classList.add('bgColor');
+    }
+  }
+  window.addEventListener('scroll',changeBg);
+   
+function change1(){
+    document.getElementById('suretik').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Keila.7002210_jungle-and-moonstruck_02_540x.jpg?v=1658848524";
+}
+function change11(){
+    document.getElementById('suretik').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Keila.7002210_jungle-and-moonstruck_01_540x.jpg?v=1658848521";
+}
+function change2(){
+    document.getElementById('suretik2').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Sunday.7002210_jungle_02_540x.jpg?v=1659349639";
+}
+function change22(){
+    document.getElementById('suretik2').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Sunday.7002210_junge_01_540x.jpg?v=1659349637";
+}
+function change3(){
+    document.getElementById('suretik3').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Tilda.7002210_lake-blue-checked_02_540x.jpg?v=1661240210";
+}
+function change33(){
+    document.getElementById('suretik3').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Tilda.7002210_lake-blue-checked_01_540x.jpg?v=1661240207";
+}
+function change4(){
+    document.getElementById('suretik4').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Thema.7002211_Jungle_02_540x.jpg?v=1661240623";
+}
+function change44(){
+    document.getElementById('suretik4').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Thema.7002211_Jungle_01_540x.jpg?v=1661240621";
+}
+function change5(){
+    document.getElementById('suretik5').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Catena.7002210_Rosette_02_540x.jpg?v=1658831340";
+}
+function change55(){
+    document.getElementById('suretik5').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Catena.7002210_Rosette_01_540x.jpg?v=1658831338";
+}
+function change6(){
+    document.getElementById('suretik6').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Keilafur.7002213_Anemone_02_540x.jpg?v=1659435971";
+}
+function change66(){
+    document.getElementById('suretik6').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Keilafur.7002213_Anemone_01_540x.jpg?v=1659435968";
+}
+function change7(){
+    document.getElementById('suretik7').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Jelene.7002210T_Misty-rose_02_540x.jpg?v=1659435943";
+}
+function change77(){
+    document.getElementById('suretik7').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Jelene.7002210T_Misty-rose_01_540x.jpg?v=1659435940";
+}
+function change8(){
+    document.getElementById('suretik8').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Keila.7002210_black-and-angora_02_540x.jpg?v=1658848463";
+}
+function change88(){
+    document.getElementById('suretik8').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Keila.7002210_black-and-angora_01_540x.jpg?v=1658848461";
+}
+function change9(){
+    document.getElementById('suretik9').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Alane.7002210_Black_02_540x.jpg?v=1665672391";
+}
+function change99(){
+    document.getElementById('suretik9').src="https://cdn.shopify.com/s/files/1/0515/3633/0928/products/Alane.7002210_Black_01_540x.jpg?v=1665672388";
+}
