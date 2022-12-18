1e362e0aff505f4d9f272785f4aab0ff51e525bb
Nurgul061 <119525325+Nurgul061@users.noreply.github.com>
2022-12-18 17:05:54 +0300

    16th

--- about.js
+++ about.js
@@ -70,3 +70,20 @@ function listNext(page) {
 
   observer.observe(square);
 })();
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
--- shop.js
+++ shop.js
@@ -33,3 +33,18 @@ setInterval(function() {
 
 }, 3000);
 
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
